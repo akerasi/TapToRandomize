e19f87ddcfe6f838e305df94c5a377b53c2af0cd
Allen Tipper <akerasi@gmail.com>
2024-08-04 17:52:12 -0400

    more robust, and figured out venv isn't always our friend.

--- randomizerlauncher.sh
+++ randomizerlauncher.sh
@@ -3,7 +3,7 @@ BaseGameDir=/media/fat/cifs/games
 BaseSnesDir=SNES
 BaseNesDir=NES
 SolarJetmanRandoDir=SolarJetmanRando
-SolarJetmanRom="$BaseGameDir/$BaseNesDir/EverDrive N8 & PowerPak v6.2/1 US - R-Z/Solar Jetman - Hunt for the Golden Warpship (USA).nes"
+SolarJetmanRom='/media/fat/cifs/games/NES/randoroms/Solar Jetman - Hunt for the Golden Warpship (USA).nes'
 BaseRandoDir=/tmp/rando/
 KeepSeeds=5
 shift_old_seeds(){
@@ -11,21 +11,26 @@ shift_old_seeds(){
         mkdir -p $BaseRandoDir/archive
         SeedInCurrent=$(ls -1 $BaseRandoDir/current | wc -l)
         if (( SeedInCurrent > 0 )); then
-                mv $BaseRandoDir/current/* $BaseRandoDir/archive
+                for filename in $BaseRandoDir/current/*; do
+                        echo "$filename"
+                        mv "$filename" $BaseRandoDir/archive
+                done
                 cd $BaseRandoDir/archive
                 ls -1tr | head -n -$KeepSeeds | xargs -d '\n' rm -f --
+                cd /media/fat/Scripts
         fi;
 }
 case $1 in
         solarjetman)
+                BaseRandoDir=$BaseGameDir/$BaseNesDir/$SolarJetmanRandoDir
+                shift_old_seeds
                 if [ ! -e .venvsj/bin/activate ]; then
                     python -m venv .venvsj
                 fi
                 source .venvsj/bin/activate
                 pip install sj-rando
-                BaseRandoDir=$BaseGameDir/$BaseNesDir/$SolarJetmanRandoDir
-                shift_old_seeds
-                sj-rando --rompath "$SolarJetmanRom"
+                sj-rando --rompath "${SolarJetmanRom}"
+                deactivate
                 mv *.nes "$BaseRandoDir/current"
         ;;
 
